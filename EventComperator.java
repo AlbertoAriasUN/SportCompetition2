2b910344195825f74ffc1d777a468a8653ef97fc
rosannak <34712418+rosannak@users.noreply.github.com>
2018-01-30 10:22:05 +0100

    Delete EventComperator.java

--- EventComperator.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.Comparator;
-
-public class EventComperator implements Comparator<Result> {
-
-    public int compare(Result o1, Result o2) {
-        return o1.getSportEvent().getEventName().compareTo(o2.getSportEvent().getEventName());
-    }
